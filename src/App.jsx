ecbab26d10e6a7b74238b3a781b6032e247d043b
Jorge Mayta <luismayta08@gmail.com>
2023-01-16 04:10:10 -0500

    instalacion de paquetes y configuracion de archivos jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { RouterProvider } from 'react-router-dom';
-import './App.css';
-import { primaryRoute } from './routes/primaryRoute';
+import {  Routes, Route, BrowserRouter } from "react-router-dom";
+import ShowProducts from './components/ShowProducts';
 
 function App() {
   return (
-    <RouterProvider router={primaryRoute} />
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/store-vite-raect/' element={<ShowProducts />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
